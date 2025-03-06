0bd1d7cdd95648783a8d6cf3f54ccb5a6166f130
Daniel Villafane <villafapd@outlook.com>
2025-03-06 09:56:34 -0300

    Update

--- main.py
+++ main.py
@@ -266,8 +266,8 @@ if __name__ == "__main__":
 	schedule.every().day.at("18:00").do(buscar_epg)
 	schedule.every().day.at("18:30").do(buscar_epg_redbulltv)
 	schedule.every().day.at("23:00").do(buscar_epg_redbulltv)
-	#uscar_epg()
-	#buscar_epg_redbulltv()
+	uscar_epg()
+	buscar_epg_redbulltv()
 	try:
 		while True:
 			schedule.run_pending()
